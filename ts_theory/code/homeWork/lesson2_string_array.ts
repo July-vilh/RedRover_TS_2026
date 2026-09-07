0b8a7f5d969e9e638cd9f306fe7cf239b5ba002f
julyvilh <106986983+July-vilh@users.noreply.github.com>
2026-09-07 21:33:11 +0200

    lesson2_task6

--- ts_theory/code/homeWork/lesson2_string_array.ts
+++ ts_theory/code/homeWork/lesson2_string_array.ts
@@ -214,17 +214,6 @@ https://docs.google.com/spreadsheets/d/1f-S88J_MQZ_g53BhIgPEbLvz3pnkQzkqC42S8P0b
 5. Область тестирования программных продуктов активно развивается и появляются новые
 названия профессии тестировщик. С помощью какого метода можно добавить название профессии
 "SDET" и "Lead SDET" в конец массива testerPositions? Напишите решение и распечатайте результат.
-
-
-const testerPositions: string[] = [
-    "Quality Assurance Engineer",
-    "Software Tester",
-    "Test Automation Engineer",
-    "Quality Analyst",
-    "QA Tester",
-    "Test Engineer",
-    "Quality Control Analyst",
-];
 */
 
 
@@ -238,14 +227,8 @@ const testerPositions: string[] = [
     "Quality Control Analyst",
 ];
 
-
-console.log(testerPositions);
 testerPositions.push("SDET", "Lead SDET");
 console.log(testerPositions);
-
-
-
-
 
 
 /*
@@ -259,98 +242,102 @@ console.log(testerPositions);
 */
 
 
-let day: string = "Monday";
+let day: string = "Monday"; //исправлена ошибка "monday" -> "Monday"
 let message: string;
 
-
-day = day.toLowerCase();
-
-
-if (day === "monday") {
-    message = "It's Monday!";
-}
-else if (day === "tuesday") {
-    message = "It's Tuesday!";
-}
-else if (day === "wednesday") {
-    message = "It's Wednesday!";
-}
-else if (day === "thursday") {
-    message = "It's Thursday!";
-}
-else if (day === "friday") {
-    message = "It's Friday!";
-}
-else if (day === "saturday") {
-    message = "It's Saturday!";
-}
-else if (day === "sunday") {
-    message = "It's Sunday!";
-}
+if (day === "Monday") {
+     message = "It's Monday!";
+} 
+else if (day === "Tuesday") { //исправлена ошибка "TUesday" -> "Tuesday"
+     message = "It's Tuesday!";
+} 
+else if (day === "Wednesday") {
+     message = "It's Wednesday!";
+} 
+else if (day === "Thursday") { //исправлена ошибка "thursday" -> "Thursday"
+     message = "It's Thursday!";
+} 
+else if (day === "Friday") {
+     message = "It's Friday!";
+} 
+else if (day === "Saturday") {
+     message = "It's Saturday!"; //исправлена ошибка "Its Sunday!" -> "It's Saturday!"
+} 
+else if (day === "Sunday") {
+     message = "It's Sunday!";
+} 
 else {
-    message = "Invalid day!";
+     message = "Invalid day!";
 }
-
 
 console.log(message);
 
 
+// Вариант 2 (решение преподавателя): вместо подгонки переменной под сравнения
+// приводим вход к нижнему регистру - тогда работает любой регистр: "Monday", "monday", "MONDAY".
+// Блок { } нужен, чтобы имена day и message не конфликтовали с объявленными выше.
+{
+    let day: string = "Monday";
+    let message: string;
 
+    day = day.toLowerCase();
 
+    if (day === "monday") {
+        message = "It's Monday!";
+    }
+    else if (day === "tuesday") {
+        message = "It's Tuesday!";
+    }
+    else if (day === "wednesday") {
+        message = "It's Wednesday!";
+    }
+    else if (day === "thursday") {
+        message = "It's Thursday!";
+    }
+    else if (day === "friday") {
+        message = "It's Friday!";
+    }
+    else if (day === "saturday") {
+        message = "It's Saturday!";
+    }
+    else if (day === "sunday") {
+        message = "It's Sunday!";
+    }
+    else {
+        message = "Invalid day!";
+    }
 
+    console.log(message);
+}
 
 /*
 7. В этом коде ошибки. Найдите, покажите какие и почините.
-
-
-- Переменная result3 не может быть создана через const
-- Все значения в case кроме 5 и 6 - строки, а переменная monthNumber - число.
-- Case со значением 3 - забыли break
-- Case со значением 5 - переменная results3, а не result3 (лишняя буква s)
-- Case со значением 6 - забыли двоеточие
-- Case со значением 6 - используем = (присваивание), а не == (сравнивание)
-- Нет дефолтного значения
 */
 
-
 const monthNumber: number = 1;
-let result3: string = "";
-
+const result3: string = "";
 
 switch (monthNumber) {
-    case 1:
-        result3 = "January";
-        break;
-    case 2:
-        result3 = "February";
-        break;
-    case 3:
-        result3 = "March";
-        break;
-    case 4:
-        result3 = "April";
-        break;
-    case 5:
-        result3 = "May";
-        break;
-    case 6:
-        result3 = "June";
-        break;
-    default:
-        result3 = "Not Specified";
-        break;
+     case "1":
+          result3 = "January";
+          break;
+     case "2":
+          result3 = "February";
+          break;
+     case "3":
+          result3 = "March";
+     case "4":
+          result3 = "April";
+          break;
+     case 5:
+          results3 = "May";
+          break;
+     case 6
+          result3 == "June";
+          break;
 }
 
-
 console.log(result3);
-
-
-
-
-
-
-
-
 
 
 /*
@@ -359,69 +346,11 @@ console.log(result3);
 */
 
 
-const dayId: number = 1;
-let dayName: string = "";
-let error: boolean = false;
-
-
-switch (dayId) {
-    case 1:
-        dayName = "Monday";
-        break;
-    case 2:
-        dayName = "Tuesday";
-        break;
-    case 3:
-        dayName = "Wednesday";
-        break;
-    case 4:
-        dayName = "Thursday";
-        break;
-    case 5:
-        dayName = "Friday";
-        break;
-    case 6:
-        dayName = "Saturday";
-        break;
-    case 7:
-        dayName = "Sunday";
-        break;
-    default:
-        error = true;
-        dayName = "Sorry, no such day exist!";
-        break;
-}
-
-
-if (error) {
-    console.log(dayName);
-}
-else {
-    console.log(`Today is ${dayName}.`);
-}
-
-
-
-
-
-
 /*
 9. Напишите программу по согласованию отпуска в зависимости от месяца.
    Если месяц "July" или "August", то программа должна показать - "approved",
    если другие месяцы, то - "denied".
 */
-
-
-let month: string = "July";
-month = month.toLowerCase();
-
-
-if (month === "july" || month == "august") {
-    console.log("Approved");
-}
-else {
-    console.log("Denied");
-}
 
 
 /*
@@ -432,69 +361,10 @@ else {
 */
 
 
-const num1: number = 5;
-const num2: number = 5;
-const operator: string = "+";
-let result7: string = "";
-
-
-if (typeof num1 === "number" && !isNaN(num1) && typeof num2 === "number" && !isNaN(num2)) {
-    if (operator === "+") {
-        result7 = (num1 + num2).toString();
-    }
-    else if (operator === "-") {
-        result7 = (num1 - num2).toString();
-    }
-    else if (operator === "*") {
-        result7 = (num1 * num2).toString();
-    }
-    else if (operator === "/") {
-        if (num2 === 0) {
-            result7 = "Division by 0 is not allowed!";
-        }
-        else {
-            result7 = (num1 / num2).toString();
-        }
-    }
-    else {
-        result7 = "Please, enter correct operator!";
-    }
-}
-else {
-    result7 = "Check your numbers!";
-}
-
-
-console.log(result7);
-
-
-
-
-
-
 /*
 11. Напишите программу, которая определяет, является ли заданный год високосным по правилам Григорианского календаря.
    Високосный год делится на 4, за исключением случаев, когда он делится на 100, но не на 400.
 */
-
-
-const year: number = 1900;
-let result8: boolean;
-
-
-if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-    result8 = true;
-} else {
-    result8 = false;
-}
-
-
-console.log(result8);
-
-
-
-
-
 
 /*
 12. Напишите программу, которая рассчитывает стоимость билета в зависимости от возраста.
@@ -505,80 +375,12 @@ console.log(result8);
 */
 
 
-const customerAge: number = 25;
-const origTicketPrice: number = 100;
-const isStudent: boolean = false;
-let salePrice: number = 0;
-
-
-if (customerAge > 2) {
-    if (customerAge < 10) {
-        salePrice = origTicketPrice - origTicketPrice * 0.5;
-    }
-    else if (customerAge >= 65) {
-        salePrice = origTicketPrice - origTicketPrice * 0.15;
-    }
-    else {
-        salePrice = isStudent ? origTicketPrice - origTicketPrice * 0.1 : origTicketPrice;
-    }
-}
-
-
-console.log("Ticket Price: $" + salePrice);
-
-
-
-
 /*
 13. Решите задачу с помощью пройденных тем (исключительно с помощью пройденных тем):
    Улитка ползёт вверх по стене высотой 5 метров. Каждый день она проползает
    вверх на 3 метра, а каждую ночь съезжает вниз на 2 метра. За сколько дней она
    доползет до вершины стены.
 */
-
-
-const wall: number = 5;
-const dayDistance: number = 3;
-const nightDistance: number = 2;
-let totalMeters: number = 0;
-let dayNumber: number = 0;
-
-
-// день 1
-if ((totalMeters = totalMeters + dayDistance) < wall) {  //3 метра меньше 5, иди дальше
-    console.log(`Day ${++dayNumber}, your distance is ${totalMeters} meter(s). Go forward!`);
-}
-else if (totalMeters === wall) {
-    console.log(`Day ${++dayNumber}, your distance is ${totalMeters} meter(s). You've reached your destination!`);
-}
-totalMeters = totalMeters - nightDistance; //теперь 1 метр
-
-
-// день 2
-if ((totalMeters = totalMeters + dayDistance) < wall) {  //4 метра меньше 5, иди дальше
-    console.log(`Day ${++dayNumber}, your distance is ${totalMeters} meter(s). Go forward!`);
-}
-else if (totalMeters === wall) {
-    console.log(`Day ${++dayNumber}, your distance is ${totalMeters} meter(s). You've reached your destination!`);
-}
-totalMeters = totalMeters - nightDistance; //теперь 2 метра
-
-
-// день 3
-if ((totalMeters = totalMeters + dayDistance) < wall) {  //5 метра не меньше 5, доползла!
-    console.log(`Day ${++dayNumber}, your distance is ${totalMeters} meter(s). Go forward!`);
-}
-else if (totalMeters === wall) {
-    console.log(`Day ${++dayNumber}, your distance is ${totalMeters} meter(s). You've reached your destination!`);
-}
-totalMeters = totalMeters - nightDistance;
-
-
-
-
-
-
-
 
 /*
 14. Креативное задание:
@@ -593,76 +395,6 @@ totalMeters = totalMeters - nightDistance;
 */
 
 
-const smallCup: string = "small cup";
-const mediumCup: string = "medium cup";
-const largeCup: string = "large cup";
-
-
-const hotWater: string = "hot water";
-const coldWater: string = "cold water";
-
-
-const latte: string = "Latte";
-const icedCoffee: string = "Iced Coffee";
-
-
-//make your choice here
-let price: number = 5;
-let coffeeType: string = icedCoffee;
-let cup: string = smallCup;
-let water: string = "";
-
-
-if (coffeeType === icedCoffee) {
-    water = coldWater;
-}
-else {
-    water = hotWater;
-}
-
-
-if (cup === mediumCup) {
-    price *= 1.5;
-}
-else if (cup === largeCup) {
-    price *= 2;
-}
-
-
-
-
-const step1 = `1. Take a ${cup}.`;
-const step2 = "2. Put 1 spoon of coffee in it.";
-const step3 = `3. Pour ${water} in the cup with coffee.`;
-const step4 = "4. Pour a splash of milk in the cup.";
-const step5 = "5. Stir coffee in the cup with a spoon.";
-
-
-const customerOrder = `
-Make one ${cup} of ${coffeeType}:
-${step1}
-${step2}
-${step3}
-${step4}
-${step5}
-
-
-The order price is $${price}.
-
-
-Enjoy your coffee!
-`;
-
-
-console.log(customerOrder);
-
-
-
-
-
-
-
-
 
 /*
 15. Создайте программу, которая определяет, имеет ли человек право на определенное мероприятие, на основе нескольких критериев: возраста, статуса членства и посещения предыдущих мероприятий.
@@ -672,33 +404,3 @@ console.log(customerOrder);
     - Если человек не является членом клуба, необходимо посетить не менее 3 предыдущих мероприятий.
     - Если человек не является членом клуба, но посетил 5 или больше предыдущих мероприятий, он получает специальный VIP-пропуск.  
 */
-
-
-const memberAge: number = 5;
-const isMember: boolean = false;
-const previousEventsAttended: number = 0;
-let eligibilityResult: string = "";
-
-
-if (memberAge < 18) {
-    eligibilityResult = "🔴 Not eligible: Must be at least 18 years old.";
-}
-else {
-    if (isMember) {
-        eligibilityResult = "🟢 Eligible: Member.";
-    }
-    else {
-        if (previousEventsAttended >= 5) {
-            eligibilityResult = "🟢 Eligible: VIP Pass!";
-        }
-        else if (previousEventsAttended >= 3) {
-            eligibilityResult = "🟢 Eligible: Non-member with sufficient attendance.";
-        }
-        else {
-            eligibilityResult = "🔴 Not eligible: Non-member with insufficient attendance.";
-        }
-    }
-}
-
-
-console.log(eligibilityResult);
